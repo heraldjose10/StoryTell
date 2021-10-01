5f717f3b3800a11cd1f707d737be26dcf27ec5b3
unknown <heraldjose10@gmail.com>
2021-10-01 14:53:44 +0530

    added styles to blogpage

--- blogApp/forms.py
+++ blogApp/forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Email
 
 class AuthorLogin(FlaskForm):
     email = StringField('email', validators= [DataRequired(), Email()])
     password = PasswordField('password', validators=[DataRequired()])
+    remember = BooleanField('remember me')
     login = SubmitField('Login')
