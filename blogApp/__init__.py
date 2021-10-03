b5b414ffc5157e5b2f9a5a0db55e5bfab6cc2d96
unknown <heraldjose10@gmail.com>
2021-10-03 23:00:57 +0530

    added tag search

--- blogApp/__init__.py
+++ blogApp/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dfuygiaughdauij3q72846yqiuehrgiq'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from blogApp import routes
