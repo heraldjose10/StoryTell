a2d04832cf243a53c1239684252c18ed26ddc914
unknown <heraldjose10@gmail.com>
2021-10-20 17:06:06 +0530

    added flask-moment

--- blogApp/__init__.py
+++ blogApp/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+import jinja2
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_migrate import Migrate
-from flask_assets import Environment, Bundle
+from flask_assets import Environment
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dfuygiaughdauij3q72846yqiuehrgiq'
@@ -15,9 +17,11 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "author"
 migrate = Migrate(app, db, render_as_batch=True)
+
 assets = Environment(app)
 app.config['ASSETS_DEBUG'] = True
 assets.url = app.static_url_path
 
-from blogApp import routes
-from blogApp import commands
+moment = Moment(app)
+
+from blogApp import routes, commands
