6f21012b938d98a2405c7ac4e6ec946c7e2a3bf1
heraldjose10 <heraldjose10@gmail.com>
2022-03-07 22:25:48 +0530

    add wsgi file

---
+++ production.wsgi
@@ -0,0 +1,5 @@
+import sys
+
+sys.path.insert(0, "/var/www/html/StoryTell/")
+
+from run import app as application
--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from blogApp import create_app, commands
 
-if __name__ == '__main__':
-    app = create_app()
-    commands.register(app)
-    app.run(debug=True,  port=5500, host='0.0.0.0', ssl_context = 'adhoc')
+
+app = create_app()
+commands.register(app)
+app.run()
